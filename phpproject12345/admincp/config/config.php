963a6cf1e1e30b1ef9f777b9ce09a1f6f876631a
VVK <khoavvgcd191275@fpt.edu.vn>
2021-06-08 14:44:12 +0700

    vovankhoa

--- phpproject12345/admincp/config/config.php
+++ phpproject12345/admincp/config/config.php
@@ -2,8 +2,8 @@
 
 $conn_string = "host = ec2-34-193-112-164.compute-1.amazonaws.com"
         . " port = 5432 "
-        . " dbname = d9679rdtfjpra"
-        . " user = qpcxqwxbunyfzc"
-        . " password = ed64a6f14c6ad1c1c758efed47bf5084851f4425a75353158f8736ddd55f87cb";
+        . " dbname = d5e2mp8t2a0jhm"
+        . " user = gnsbowxqjpuqxk"
+        . " password = c0ba00d00d5a9faa30cd34055546f59373e03fc45dc13b06a844df3ececc696f";
 $db = pg_connect($conn_string);
 ?>
