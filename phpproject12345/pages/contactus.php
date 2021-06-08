89de2c48762d0fabb7b7d56efaee0ab9e6b0a339
VVK <khoavvgcd191275@fpt.edu.vn>
2021-06-08 14:40:46 +0700

    vovankhoa

---
+++ phpproject12345/pages/contactus.php
@@ -0,0 +1,181 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../css/contac.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../CSSWeb/contac.css">
+</head>
+<body>
+<style>
+        body{
+  background:#ACD7E5;
+ }
+ .address-icon i{
+   font-size: 36px;
+   line-height: 32px;
+ }
+ .icons i{
+   color: #fff;
+   padding: 8px 0px;
+   text-align: center;
+   height: 30px;
+   width: 30px;
+   margin-right: 5px;
+ }
+ .fa-facebook{
+   background-color: #3A5A99; 
+ }
+ .fa-twitter{
+   background-color: #39CBFA;
+ }
+ .fa-linkedin{
+   background-color: #3D99C0;
+ }
+ .fa-github{
+   background-color: #000;
+ }
+ .footer-basic {
+  padding:40px 0;
+  background-color:#ffffff;
+  color:#4b4c4d;
+}
+
+.footer-basic ul {
+  padding:0;
+  list-style:none;
+  text-align:center;
+  font-size:18px;
+  line-height:1.6;
+  margin-bottom:0;
+}
+
+.footer-basic li {
+  padding:0 10px;
+}
+
+.footer-basic ul a {
+  color:inherit;
+  text-decoration:none;
+  opacity:0.8;
+}
+
+.footer-basic ul a:hover {
+  opacity:1;
+}
+
+.footer-basic .social {
+  text-align:center;
+  padding-bottom:25px;
+}
+
+.footer-basic .social > a {
+  font-size:24px;
+  width:40px;
+  height:40px;
+  line-height:40px;
+  display:inline-block;
+  text-align:center;
+  border-radius:50%;
+  border:1px solid #ccc;
+  margin:0 8px;
+  color:inherit;
+  opacity:0.75;
+}
+
+.footer-basic .social > a:hover {
+  opacity:0.9;
+}
+
+.footer-basic .copyright {
+  margin-top:15px;
+  text-align:center;
+  font-size:13px;
+  color:#aaa;
+  margin-bottom:0;
+}
+    </style>
+    <br><br>
+    <div class="container">
+      <div class="row">
+          <div class="col-lg-8 offset-lg-2 col-12">
+              <div class="row">
+                  <div class="col-lg-12 col-12 p-0 contact-us">
+                    <h4 class="">CONTACT US</h4><hr>
+                  </div>
+              </div>
+              <div class="row bg-light pt-3 pb-3 mb-4">
+                  <div class="col-lg-12">
+                    <h6>ADDRESS :</h6>
+                  </div>
+                  <div class="col-lg-4 col-4">
+                    02 Phan Châu, Minh An, Hội An, Quảng Nam                 
+                  </div>
+                  <div class="col-lg-4 col-4">
+                  <p class="m-0 text-danger"><i class="fa fa-phone-square" aria-hidden="true"></i>
+                      (+84) 905955162
+                  </p>
+                  <p class="m-0 text-info"><i class="fa fa-envelope" aria-hidden="true"></i>
+                      khoavvgcd191275@fpt.edu.vn
+                  </p>
+                  </div>
+              </div>
+              <div class="row bg-light pt-3 pb-3 mb-4">
+                  <div class="col-lg-6 col-12">
+                    <form>
+                      <div class="form-row mb-3">
+                        <div class="col">
+                          <input type="text" class="form-control" placeholder="Full Name:">
+                        </div>
+                        <div class="col">
+                          <input type="text" class="form-control" placeholder="Company:">
+                        </div>
+                      </div>
+                      <div class="form-row mb-3">
+                        <div class="col">
+                          <input type="text" class="form-control" placeholder="Email:">
+                        </div>
+                        <div class="col">
+                          <input type="text" class="form-control" placeholder="Phone:">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Message:"></textarea>
+                      </div>
+                      <button type="submit" class="btn btn-danger mb-4">Send</button>
+                    </form>
+                  </div>
+                  <div class="col-lg-6 col-12">
+                      <div style="width: 100%"><iframe width="100%" height="300px"
+                        src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d61341.08300724828!2d108.22590129999999!3d16.0749477!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1617355220781!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </div>
+    <footer>
+        <div class="footer-basic">
+            <footer>
+                <div class="social"><a href="https://www.instagram.com/vovankhoa_vvk/"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="https://www.facebook.com/vvk0000/"><i class="icon ion-social-facebook"></i></a></div>
+                <ul class="list-inline">
+                    <li class="list-inline-item"><a href="../index.php">Home</a></li>
+                    <li class="list-inline-item"><a href="profile.php">Profile</a></li>
+                    <li class="list-inline-item"><a href="../admincp/register.php">Register</a></li>
+                    <li class="list-inline-item"><a href="../admincp/login.php">Login</a></li>
+                    <li class="list-inline-item"><a href="../admincp/logout.php">Logout</a></li>
+                    <li class="list-inline-item"><a href="giohang.php">My Cart</a></li>
+                </ul>
+                <p class="copyright">Copy right Vo Van Khoa @ 2021</p>
+            </footer>
+        </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>   
+    </footer>
+    </body>
+</html>
