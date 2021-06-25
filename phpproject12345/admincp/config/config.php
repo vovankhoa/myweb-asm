040e4b6339386f17ea41a40462194881039e44ce
vovankhoa <75871572+vovankhoa@users.noreply.github.com>
2021-06-25 17:49:04 +0700

    Delete phpproject12345 directory

--- phpproject12345/admincp/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn_string = "host = ec2-34-193-112-164.compute-1.amazonaws.com"
-        . " port = 5432 "
-        . " dbname = d5e2mp8t2a0jhm"
-        . " user = gnsbowxqjpuqxk"
-        . " password = c0ba00d00d5a9faa30cd34055546f59373e03fc45dc13b06a844df3ececc696f";
-$db = pg_connect($conn_string);
-?>
