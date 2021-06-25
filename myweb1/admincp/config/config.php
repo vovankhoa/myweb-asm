ab63499d9a5df7cc2239c0d8cf5f2b2a70886528
vovankhoa <75871572+vovankhoa@users.noreply.github.com>
2021-06-25 17:50:59 +0700

    Delete myweb1 directory

--- myweb1/admincp/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn_string = "host = ec2-34-193-112-164.compute-1.amazonaws.com"
-        . " port = 5432 "
-        . " dbname = d9679rdtfjpra"
-        . " user = qpcxqwxbunyfzc"
-        . " password = ed64a6f14c6ad1c1c758efed47bf5084851f4425a75353158f8736ddd55f87cb";
-$db = pg_connect($conn_string);
-?>
--- myweb1/pages/sanpham.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include("admincp/config/config.php");
-$id=$_GET['idproduct'];
-    $sql="SELECT*FROM tb_product Where PID='".$id."' ";
-    $sql_query=pg_query($mysql,$sql);
-    $row_sanpham=pg_fetch_array($sql_query);
-?>
-<p><?php echo $row_sanpham['thongsokt'] ?></p>
